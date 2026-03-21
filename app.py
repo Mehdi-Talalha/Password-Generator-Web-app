c099201205a5f0e64a1289b0231bd643d47f8f9b
Mehdi Talalha <m.talalha@gmail.com>
2026-03-21 12:56:19 +0000

    Mehdi Talalha: fix used characters, index.html error, everything work well

--- app.py
+++ app.py
@@ -2,31 +2,53 @@ from flask import Flask, render_template, request
 import random
 import string
 
-def Generate_password(length):
-    # take the charcters form the string module
-    CHARACTERS = "".join(char for char in string.printable if char not in set(string.whitespace))
-    # return the random password
-    return ''.join(random.choice(CHARACTERS) for _ in range(length))
+
+def Generate_password(length, characters):
+    return ''.join(random.choice(characters) for _ in range(length))
 
 # to run the server ==> flask --app app.py run --debug --port 5001
 
 app = Flask(__name__)
 
+CHARACTER_MAP = {
+    "digits" : string.digits,
+    "ascii_letters" : string.ascii_letters,
+    "punctuation" : string.punctuation
+}
+
+char_key = ['digits', 'ascii_letters','punctuation']
+char_value = [string.digits, string.ascii_letters, string.punctuation]
+
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index():    
     if request.method == "POST":
-        length = request.form.get("length")
+        length = int(request.form.get("length"))
+        
+        characters = ""
+
+        for i in range(len(char_key)):
+            if request.form.get(char_key[i]):
+                characters += char_value[i]
+                
+        if not characters:
+            # trmember to remove whitespaces
+            characters = string.printable
+            
         # handle if the length exist and not agreed
         if length:
             try:
-                length = int(length)
-                password = Generate_password(length)
-                return render_template("index.html", password=password, length=length)
+                if length < 8:
+                    return render_template("index.html", error="Length should be greater than 8")
+                elif length > 50:
+                    return render_template("index.html", error="Length should be shorter than 50.")
+                else:
+                    password = Generate_password(length, characters)
+                    return render_template("index.html", password=password, length=length)
             except ValueError:
-                return render_template("index.html",error="Please enter a valid number")
+                return render_template("index.html",error="Please enter a number.")
         else:
             # handle if the length doesn't exist
-            return render_template("index.html", error="Please enter the length of the number")
+            return render_template("index.html", error="Please enter the length.")
     # return the default website of they are no length recived
     return render_template("index.html")
 
--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-import string, random
+char = "rzgzeg"
 
-characters = string.ascii_letters
-
-
-def Generate_password(n, characters):
-    pwd = []
-    for i in range(10):
-        pwd[i] = random.choice(characters)
-    return "".join(pwd)
-
-print(Generate_password(10, characters))
+if not char:
+    print("hello wolrd")
+else:
+    print("not hello world")
